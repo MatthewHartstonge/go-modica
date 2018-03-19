3a8d83d4cdf4ea1bd04cd65213bcf7cf252d7c60
Matthew Hartstonge <matt@mykro.co.nz>
2018-03-19 23:12:50 +1300

    :new: Initial Commit

---
+++ mobile_gateway.go
@@ -0,0 +1,92 @@
+package modica
+
+import (
+	"fmt"
+	"strconv"
+)
+
+const (
+	baseMessagePath          = "/messages"
+	baseBroadcastMessagePath = baseMessagePath + "/broadcast"
+)
+
+// MobileGatewayService implements modica's Mobile Gateway HTTPS API v2
+type MobileGatewayService service
+
+// CreateMessage sends an (outbound) message to a single destination.
+func (m MobileGatewayService) CreateMessage(newMessage *Message) (message *Message, err error) {
+	req, err := m.client.newRequest(methodPost, baseMessagePath, newMessage)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = m.client.do(req, &message)
+	return message, err
+}
+
+// GetMessage retrieves a message
+func (m MobileGatewayService) GetMessage(messageID int) (message *Message, err error) {
+	uri := fmt.Sprintf("%s/%s", baseMessagePath, strconv.Itoa(messageID))
+	req, err := m.client.newRequest(methodGet, uri, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = m.client.do(req, &message)
+	return message, err
+}
+
+// CreateBroadcastMessage sends an (outbound) message to multiple destinations
+func (m MobileGatewayService) CreateBroadcastMessage(newMessage *BroadcastMessage) (message *BroadcastMessage, err error) {
+	req, err := m.client.newRequest(methodPost, baseBroadcastMessagePath, newMessage)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = m.client.do(req, &message)
+	return message, err
+}
+
+// Message provides the data model to unmarshal and marshal a single message for Modica's mobile gateway API.
+type Message struct {
+	// ID contains the message ID
+	ID int `json:"id,omitempty"`
+
+	// Required Attributes
+	// Destination contains the destination mobile number the SMS message will be sent to.
+	// Number format must be international format e.g. +64211234567 / +61414123456 / +18123456789.
+	Destination string `json:"destination"`
+
+	// Content contains the text to be sent verbatim to the mobile phone.
+	Content string `json:"content"`
+
+	// Optional Attributes.
+	// Source allows you to select the source short code or mobile number where you have multiple available.
+	// This parameter is optional but when it is used it must be used instead of the class parameter.
+	Source string `json:"source,omitempty"`
+
+	// Scheduled enables a message to be sent at a scheduled time.
+	Scheduled string `json:"scheduled,omitempty"`
+
+	// Reference is unknown.
+	Reference string `json:"reference,omitempty"`
+
+	// Class allows you to define the type of message.
+	// Classes are used to determine how to route and bill the message, as well as provide useful reporting information.
+	// Modica will supply you with set of valid classes available to your account. In most cases this should be mt_message.
+	// If the source parameter is used then class must not be included.
+	Class string `json:"class,omitempty"`
+
+	// Mask is unknown.
+	Mask string `json:"mask,omitempty"`
+
+	// SMSClass is unknown, but must be between 1-3.
+	SMSClass int `json:"sms_class,omitempty"`
+}
+
+// BroadcastMessage provides the data model to unmarshal and marshal multiple messages for Modica's mobile gateway API.
+type BroadcastMessage struct {
+	Destination []string `json:"destination"`
+
+	Message
+}
---
+++ modica.go
@@ -0,0 +1,106 @@
+package modica
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+const (
+	defaultBaseURL = "https://api.modicagroup.com/rest/gateway"
+	userAgent      = "go-modica"
+)
+
+const (
+	methodPost = "POST"
+	methodGet  = "GET"
+)
+
+const (
+	mediaTypeV1 = "application/vnd.modica.gateway.v1+json"
+)
+
+// Client enables talking to Modica's API
+type Client struct {
+	client *http.Client // HTTP client used to communicate with the API.
+
+	// Base URL for API requests. Defaults to the public Modica API.
+	// BaseURL should always be specified with a trailing slash.
+	BaseURL *url.URL
+
+	// User agent used when communicating with the Modica API.
+	UserAgent string
+
+	common service // Reuse a single struct instead of allocating one for each service on the heap.
+
+	// Services used for talking to different parts of the Modica API.
+	MobileGateway *MobileGatewayService
+}
+
+type service struct {
+	client *Client
+}
+
+// NewClient returns a new Modica API client. If a nil httpClient is
+// provided, http.DefaultClient will be used.
+func NewClient(httpClient *http.Client) *Client {
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+	baseURL, _ := url.Parse(defaultBaseURL)
+
+	c := &Client{client: httpClient, BaseURL: baseURL, UserAgent: userAgent}
+	c.common.client = c
+
+	// Services
+	c.MobileGateway = (*MobileGatewayService)(&c.common)
+
+	return c
+}
+
+func (c *Client) newRequest(method string, urlPath string, body interface{}) (*http.Request, error) {
+	if !strings.HasSuffix(c.BaseURL.Path, "/") {
+		return nil, fmt.Errorf("BaseURL must have a trailing slash, but %q does not", c.BaseURL)
+	}
+	u, err := c.BaseURL.Parse(urlPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var buf io.ReadWriter
+	if body != nil {
+		buf = new(bytes.Buffer)
+		err = json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	if body != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+	req.Header.Set("Accept", mediaTypeV1)
+	if c.UserAgent != "" {
+		req.Header.Set("User-Agent", c.UserAgent)
+	}
+	return req, nil
+}
+
+func (c *Client) do(req *http.Request, v interface{}) (*http.Response, error) {
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	err = json.NewDecoder(resp.Body).Decode(v)
+	return resp, err
+}
